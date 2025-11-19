87a9f1032a903271e23585d1329917463657cf8b
BairroAlto <testemunhopublicobairroalto@gmail.com>
2025-11-19 18:17:10 +0000

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
+# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
-python -m playwright install --with-deps
+# Install Playwright's browser binaries AND the necessary
+# operating system dependencies for them to run.
+playwright install --with-deps
