b5ddae876479376abded86933afc22f5d8fbe89b
BairroAlto <testemunhopublicobairroalto@gmail.com>
2025-12-23 21:13:43 +0000

    Update bible-data.js

--- js/bible-data.js
+++ js/bible-data.js
@@ -41,9 +41,9 @@ export const BIBLE_DATA = [
     { id: 38, nome: "Zacarias", caps: 14, abrev: "Zac", versiculos: [21, 13, 10, 14, 11, 15, 14, 23, 17, 12, 17, 14, 9, 21] },
     { id: 39, nome: "Malaquias", caps: 4, abrev: "Mal", versiculos: [14, 17, 18, 6] },
     { id: 40, nome: "Mateus", caps: 28, abrev: "Mat", versiculos: [25, 23, 17, 25, 48, 34, 29, 34, 38, 42, 30, 50, 58, 36, 39, 28, 27, 35, 30, 34, 46, 46, 39, 51, 46, 75, 66, 20] },
-    { id: 41, nome: "Marcos", caps: 16, abrev: "Mar", versiculos: [45, 28, 35, 41, 43, 56, 37, 38, 50, 52, 33, 44, 37, 72, 47, 20] },
+    { id: 41, nome: "Marcos", caps: 16, abrev: "Mar", versiculos: [45, 28, 35, 41, 43, 56, 37, 38, 50, 52, 33, 44, 37, 72, 47, 8] }, // Cap 16 alterado para 8
     { id: 42, nome: "Lucas", caps: 24, abrev: "Luc", versiculos: [80, 52, 38, 44, 39, 49, 50, 56, 62, 42, 54, 59, 35, 35, 32, 31, 37, 43, 48, 47, 38, 71, 56, 53] },
-    { id: 43, nome: "João", caps: 21, abrev: "João", versiculos: [51, 25, 36, 54, 47, 71, 53, 59, 41, 42, 57, 50, 38, 31, 27, 33, 26, 40, 42, 31, 25] },
+    { id: 43, nome: "João", caps: 21, abrev: "João", versiculos: [51, 25, 36, 54, 47, 71, 53, 48, 41, 42, 57, 50, 38, 31, 27, 33, 26, 40, 42, 31, 25] }, // Cap 8 alterado para 48 (versículos 12-59); Cap 9 mantido 41
     { id: 44, nome: "Atos", caps: 28, abrev: "Atos", versiculos: [26, 47, 26, 37, 42, 15, 60, 40, 43, 48, 30, 25, 52, 28, 41, 40, 34, 28, 41, 38, 40, 30, 35, 27, 27, 32, 44, 31] },
     { id: 45, nome: "Romanos", caps: 16, abrev: "Rom", versiculos: [32, 29, 31, 25, 21, 23, 25, 39, 33, 21, 36, 21, 14, 23, 33, 27] },
     { id: 46, nome: "1 Coríntios", caps: 16, abrev: "1 Cor", versiculos: [31, 16, 23, 21, 13, 20, 40, 13, 27, 33, 34, 31, 13, 40, 58, 24] },
