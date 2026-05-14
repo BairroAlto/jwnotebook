e4e0a7ca6aa3c44c711ce5cc72ea730c095cd802
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-05-14 01:10:15 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAZmDPPbqyfnP3rfrT2-xsWg92qbbL2a-0",
+    authDomain: "jwnotebook.firebaseapp.com",
+    projectId: "jwnotebook",
+    storageBucket: "jwnotebook.firebasestorage.app",
+    messagingSenderId: "299467134440",
+    appId: "1:299467134440:web:7b25d02c77fd09711f405d",
+    databaseURL: "https://jwnotebook-default-rtdb.europe-west1.firebasedatabase.app"
+};
