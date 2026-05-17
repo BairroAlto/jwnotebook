779346722c0da00ae732c88602fad7652a136f96
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-05-18 00:20:34 +0100

    Delete components directory

--- components/biblioteca-brain/biblio-dossie.js
+++
@@ -1,6 +0,0 @@
-// components/biblioteca-brain/biblio-dossie.js
-import { iniciarVisualizacaoDossie } from '../brain-core/brain-engine.js';
-
-export function render(estudo, container, db, auth) {
-    iniciarVisualizacaoDossie("Biblioteca", estudo, container, db, auth);
-}
