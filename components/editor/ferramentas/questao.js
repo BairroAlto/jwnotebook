334bd011c82ea7093cd1847e93e29ee6472d27c5
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-05-17 20:24:27 +0100

    Update questao.js

--- components/editor/ferramentas/questao.js
+++ components/editor/ferramentas/questao.js
@@ -82,10 +82,10 @@ header.querySelector('.btn-parabolica').onclick = () => {
     `;
 
     // Ajuste automático de altura para o título
-    const ajustarAlturaTitulo = () => {
-        inputTitulo.style.height = 'auto';
-        inputTitulo.style.height = inputTitulo.scrollHeight + 'px';
-    };
+   const ajustarAlturaTitulo = () => {
+    inputTitulo.style.height = 'auto';
+    inputTitulo.style.height = (inputTitulo.scrollHeight + 2) + 'px'; 
+};
 
     inputTitulo.addEventListener("input", (e) => {
         ajustarAlturaTitulo();
@@ -110,10 +110,10 @@ if (caixa.destaques) corTxt = "#000";
         font-family: inherit; line-height: 1.6; transition: background-color 0.3s;
     `;
 
-    const ajustarAlturaCorpo = () => {
-        corpo.style.height = 'auto'; 
-        corpo.style.height = corpo.scrollHeight + 'px'; 
-    };
+  const ajustarAlturaCorpo = () => {
+    corpo.style.height = 'auto'; 
+    corpo.style.height = (corpo.scrollHeight + 2) + 'px'; 
+};
 
     corpo.addEventListener("input", (e) => {
         ajustarAlturaCorpo(); 
@@ -122,10 +122,10 @@ if (caixa.destaques) corTxt = "#000";
     });
 
     // Ajustes de altura iniciais
-    setTimeout(() => {
-        ajustarAlturaTitulo();
-        ajustarAlturaCorpo();
-    }, 50);
+setTimeout(() => {
+    ajustarAlturaTitulo();
+    ajustarAlturaCorpo();
+}, 150);
 
     caixaDiv.appendChild(header);
     caixaDiv.appendChild(inputTitulo);
