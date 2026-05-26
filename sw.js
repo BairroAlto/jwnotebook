a53e7e0e8302e4fbd818a354c783cc5cff59c03d
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-05-26 13:18:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,31 @@
-const CACHE_NAME = 'notabook-x-v1';
+const CACHE_NAME = 'notabook-x-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles/global.css',
-  '/styles/typography.css',
-  '/styles/mobile.css',
-  '/styles/loading-sentinela.css',
-  '/firebase-config.js'
+  './',
+  './index.html',
+  './styles/global.css',
+  './styles/typography.css',
+  './styles/mobile.css',
+  './firebase-config.js'
 ];
 
-// Instalação: Cacheia o esqueleto do site
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Interpela as chamadas de rede
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).then((response) => {
+        // Se for um componente HTML ou JS novo, guarda em cache dinamicamente
+        return caches.open(CACHE_NAME).then((cache) => {
+          if (e.request.url.startsWith(location.origin)) {
+             cache.put(e.request, response.clone());
+          }
+          return response;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
   );
 });
