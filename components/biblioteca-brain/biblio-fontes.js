aca710e38d997ae4c9840e59ada23ccd47fa2321
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-06-05 13:18:12 +0100

    Delete components directory

--- components/biblioteca-brain/biblio-fontes.js
+++
@@ -1,6 +0,0 @@
-// components/biblioteca-brain/biblio-fontes.js
-import { iniciarVisualizacaoFontes } from '../brain-core/brain-engine.js';
-
-export function render(estudo, container, db, auth) {
-    iniciarVisualizacaoFontes("Biblioteca", estudo, container, db, auth);
-}
