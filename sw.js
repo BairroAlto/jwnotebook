b9a66331380641cf511375689bc73d8cccebe1a9
BairroAlto <testemunhopublicobairroalto@gmail.com>
2026-08-02 22:43:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'notabook-x-v36';
+// Incrementar esta versão força a criação de uma cache limpa em todos os clientes.
+const CACHE_NAME = 'notabook-x-v37-cache-reset';
 
 const PRE_CACHE_ASSETS = [
   './',
@@ -35,7 +36,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
 
